e68329eafd35a78abcb1db7aee7029a7de7f47a7
jnozsc <jnozsc@gmail.com>
2014-03-23 13:41:22 -0400

    new answer

---
+++ chapter_09/9.5.cpp
@@ -0,0 +1,26 @@
+// from leetcode
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int>& num) {
+        vector<vector<int> > result;
+        vector<int> currentPermutaions;
+        permuteRecursion(result, currentPermutaions, num, 0, num.size());
+        return result;
+    }
+
+    void permuteRecursion(vector<vector<int> >& result, vector<int> currentPermutaions, vector<int> num, int currentSize, int size) {
+        if (currentSize == size) {
+            result.push_back(currentPermutaions);
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            if (currentPermutaions.empty() ||
+                    find(currentPermutaions.begin(), currentPermutaions.end(), num[i]) == currentPermutaions.end()) {
+                currentPermutaions.push_back(num[i]);
+                permuteRecursion(result, currentPermutaions, num, currentSize + 1, size);
+                currentPermutaions.pop_back();
+            }
+        }
+
+    }
+};
